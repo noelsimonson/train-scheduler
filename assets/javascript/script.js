69b0f96290fa179839dbaf8345622a06769cfed0
Noel Simonson <noelsimonson@Nsimonson.home>
2017-03-12 11:10:00 -0400

    stylying

---
+++ assets/javascript/script.js
@@ -0,0 +1,11 @@
+
+
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyAd_YQjUHZZpxhkZduaheKMWCzk-PnY_oU",
+    authDomain: "train-b1da4.firebaseapp.com",
+    databaseURL: "https://train-b1da4.firebaseio.com",
+    storageBucket: "train-b1da4.appspot.com",
+    messagingSenderId: "121541857855"
+  };
+  firebase.initializeApp(config);
