2e9885a943300c8730ffc79391d4580c74abeed7
Noel Simonson <noelsimonson@Nsimonson.home>
2017-03-27 23:03:05 -0400

    formatting fix

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,5 +1,4 @@
-
-  // Initialize Firebase
+// Initialize Firebase
   var config = {
     apiKey: "AIzaSyAd_YQjUHZZpxhkZduaheKMWCzk-PnY_oU",
     authDomain: "train-b1da4.firebaseapp.com",
@@ -12,7 +11,6 @@
   // create var to ref database
   var database = firebase.database();
 
-
 // Default values 
   var trainName = "";
   var destination = "";
@@ -23,59 +21,59 @@
 $("#submit").on("click", function(event) {
 	event.preventDefault();
 
-// capture value in form fields and trim white space and add to divs in table
+	// capture value in form fields and trim white space and add to divs in table
 
 	trainName = $("#trainName").val().trim();
 	destination = $("#destination").val().trim();
 	firstTrainTime = $("#firstTrainTime").val().trim();
 	frequency = $("#frequency").val().trim();
 
-// First Time (pushed back 1 year to make sure it comes before current time)
-    var firstTimeConverted = moment(firstTrainTime, "hh:mm").subtract(1, "years");
-    console.log(firstTimeConverted);
+	// First Time (pushed back 1 year to make sure it comes before current time)
+	    var firstTimeConverted = moment(firstTrainTime, "hh:mm").subtract(1, "years");
+	    //console.log(firstTimeConverted);
 
-    // Current Time
-    var currentTime = moment();
-    console.log("CURRENT TIME: " + moment(currentTime).format("hh:mm"));
+	    // Current Time
+	    var currentTime = moment();
+	    //console.log("CURRENT TIME: " + moment(currentTime).format("hh:mm"));
 
-    // Difference between the times
-    var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
-    console.log("DIFFERENCE IN TIME: " + diffTime);
+	    // Difference between the times
+	    var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
+	    //console.log("DIFFERENCE IN TIME: " + diffTime);
 
-    // Time apart (remainder)
-    var tRemainder = diffTime % frequency;
-    console.log(tRemainder);
+	    // Time apart (remainder)
+	    var tRemainder = diffTime % frequency;
+	    //console.log(tRemainder);
 
-    // Minute Until Train
-    var tMinutesTillTrain = frequency - tRemainder;
-    console.log("MINUTES TILL TRAIN: " + tMinutesTillTrain);
+	    // Minute Until Train
+	    var tMinutesTillTrain = frequency - tRemainder;
+	    //console.log("MINUTES TILL TRAIN: " + tMinutesTillTrain);
 
-    // Next Train
-    var nextTrain = moment().add(tMinutesTillTrain, "minutes");
-    console.log("ARRIVAL TIME: " + moment(nextTrain).format("hh:mm"));
+	    // Next Train
+	    var nextTrain = moment().add(tMinutesTillTrain, "minutes");
+	    nextTrain = moment(nextTrain).format("hh:mm");
+	    //console.log("ARRIVAL TIME: " + moment(nextTrain).format("hh:mm"));
 
-//database push to divs
+		//database push to divs
 
 		var trainAdded = { 
-		trainName: trainName,
-		destination: destination,
-		firstTrainTime: firstTrainTime,
-		frequency: frequency,
-		nextTrain: nextTrain,
-		MinutesTillTrain: tMinutesTillTrain
-			
-			};
+			trainName: trainName,
+			destination: destination,
+			frequency: frequency,
+			nextTrain: nextTrain,
+			MinutesTillTrain: tMinutesTillTrain
+		};
+
+		console.log(trainAdded);
 
 		database.ref().push(trainAdded);
 
-		});
+});
 
 
-//listens for when input is submitted to database
+		//listens for when input is submitted to database
 		
 		database.ref().on("child_added", function(snapshot) {
-			console.log(snapshot.val().trainName);
-
+			console.log(snapshot.val());
 
 			var name = snapshot.val().trainName;
 			var place = snapshot.val().destination;
@@ -84,24 +82,12 @@ $("#submit").on("click", function(event) {
 			var next = snapshot.val().nextTrain;
 			var min = snapshot.val().MinutesTillTrain;
 
+			// add input to table
 
-// add input to table
-
-			$("#form").append(
+			$("#trainList").append(
 				"<tbody><tr><td>" + name + "</td>" +
 				"<td>" + place + "</td>" +
-				"<td>" + time + "</td>" +
+				"<td>" + freq + "</td>" +
 				"<td>" + next + "</td>" + 
 				"<td>" + min + "</td></tr></tbody>");
-		
-		});
-
-
-
-
-
-
-
-
-
-
+			});
